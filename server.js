712293605006c6a34a8a7a215824b0f782b16d52
Yeison Caicedo <yeison.caicedo@fortispay.com>
2025-10-04 17:25:06 -0500

    running with node

---
+++ server.js
@@ -0,0 +1,18 @@
+import express from "express";
+import path from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// Serve all static files (HTML, CSS, JS)
+app.use(express.static(__dirname));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "index.html"));
+});
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
